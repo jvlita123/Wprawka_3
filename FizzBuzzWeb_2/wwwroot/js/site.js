8cf331362d097e88d48fc8f13f333de380f7924c
Julita Felczuk <80294@student.pb.edu.pl>
2022-03-20 16:17:22 +0100

    Dodaj pliki projektów.

---
+++ FizzBuzzWeb_2/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let number = document.getElementById("number").value;
+let txt = "";
+
+
+if (number % 3 == 0 && number % 5 == 0) {
+    text = "fizzBuzz";
+}
+else if (number % 5 == 0) {
+    text = "Buzz";
+}
+else if (number % 3 == 0) {
+    text = "Fizz";
+}
+else {
+    text = "Liczba: <podana przez użytkownika liczba> nie spełnia kryteriów FizzBuzz";
+}
+document.getElementById("txt").innerHTML = text;
